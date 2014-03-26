40a4c402ffa8787497a1bfca4b162e575662c894
emag <lanabe.lanabe@gmail.com>
2014-03-26 12:39:14 +0900

    Fix direct requests to HTML5 mode links

--- html5app-server.js
+++ html5app-server.js
@@ -4,5 +4,13 @@ var express = require('express'),
 //initialize static server that will spit out contents of public folder
 app.use('/', express.static(__dirname + '/public'));
 
+//send our main angular html file if any link without dot is requested, e.g. 'http://someurl/about'
+//this is our actual server side redirect, we don't send index.html when there's dot in link assuming such a request
+//is for static data like .js, .css or .html
+app.get('/[^\.]+$', function(req, res){
+    res.set('Content-Type', 'text/html')
+        .sendfile(__dirname + '/public/index.html');
+});
+
 app.listen(9823); //the express server will start on port 9823
 console.log('started');
